77e0cd3901a89f1e5ad0e37e55d2272f9ec0e7a8
e0035165 <eeenginmaster95@gmail.com>
2023-05-01 11:23:08 +0800

    Initial Commit

---
+++ MinimumCostPath/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MinimumCostPath
+//
+//  Created by Sart Way on 1/5/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
